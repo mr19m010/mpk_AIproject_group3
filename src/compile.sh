cf5f40bda121927daa812e4139cb65c1a900a2ba
Christoph Mölzer <christoph.moelzer@technikum-wien.at>
2020-02-14 11:25:25 +0100

    CM: added sh.compile

---
+++ src/compile.sh
@@ -0,0 +1,8 @@
+g++ -g -o server server.cpp masterCube.cpp
+g++ -g -o client client.cpp masterCube.cpp
+
+#g++ -g -o main src/main.cpp src/masterCube.cpp
+
+# Makefile muss noch geschrieben werden
+# ./server
+# ./client
