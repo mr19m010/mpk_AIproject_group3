c2b6a9e98b7a28752e03c12ae724f87e92ff4863
Christoph Mölzer <christoph.moelzer@technikum-wien.at>
2020-02-18 19:19:39 +0100

    added folders server_exe and client_exe

--- compile.sh
+++ compile.sh
@@ -3,7 +3,11 @@
 
 
 #g++ -g -o main src/main.cpp src/masterCube.cpp src/client.cpp src/server.cpp
+echo "compiling..."
 g++ -pg -g -o main src/main.cpp src/masterCube.cpp src/client.cpp src/server.cpp
+cp main server_exe/
+cp main client_exe/
+
 
 # Makefile muss noch geschrieben werden
 # ./server
