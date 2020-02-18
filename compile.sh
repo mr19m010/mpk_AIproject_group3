c4a050cbb3dc6244e4830e65721b3af350a25cfb
Christoph Mölzer <christoph.moelzer@technikum-wien.at>
2020-02-18 19:29:28 +0100

    compile.sh update

--- compile.sh
+++ compile.sh
@@ -5,6 +5,8 @@
 #g++ -g -o main src/main.cpp src/masterCube.cpp src/client.cpp src/server.cpp
 echo "compiling..."
 g++ -pg -g -o main src/main.cpp src/masterCube.cpp src/client.cpp src/server.cpp
+mkdir -v -p server_exe
+mkdir -v -p client_exe
 cp main server_exe/
 cp main client_exe/
 
